4d3d8e02fcfb29dba9a55810cd2b51598607b7ae
KangGuYong <rivenine123@naver.com>
2024-12-06 16:23:31 +0900

    12장 완성 api삭제

--- src/api/kakaoApi.js
+++ src/api/kakaoApi.js
@@ -2,47 +2,44 @@ import axios from "axios";
 
 import { API_SERVER_HOST } from "./todoApi";
 
-const rest_api_key =`9303b460e75e7a9d28f54dd629b1c18f` //REST키값 
+const rest_api_key = ``; //REST키값
 
-const redirect_uri =`http://localhost:3000/member/kakao`
+const redirect_uri = `http://localhost:3000/member/kakao`;
 
-const auth_code_path = `https://kauth.kakao.com/oauth/authorize`
+const auth_code_path = `https://kauth.kakao.com/oauth/authorize`;
 
-const access_token_url =`https://kauth.kakao.com/oauth/token` //추가 
+const access_token_url = `https://kauth.kakao.com/oauth/token`; //추가
 
 export const getKakaoLoginLink = () => {
-
   const kakaoURL = `${auth_code_path}?client_id=${rest_api_key}&redirect_uri=${redirect_uri}&response_type=code`;
 
-  return kakaoURL
-
-}
+  return kakaoURL;
+};
 
 export const getAccessToken = async (authCode) => {
-
   const header = {
-   headers: {
-     "Content-Type": "application/x-www-form-urlencoded",
-   }
-  }
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+  };
   const params = {
     grant_type: "authorization_code",
     client_id: rest_api_key,
     redirect_uri: redirect_uri,
-    code:authCode
-  }
-  
-  const res = await axios.post(access_token_url, params , header)
+    code: authCode,
+  };
 
-  const accessToken = res.data.access_token
+  const res = await axios.post(access_token_url, params, header);
 
-  return accessToken
-}
+  const accessToken = res.data.access_token;
 
-export const getMemberWithAccessToken = async(accessToken) => {
+  return accessToken;
+};
 
-  const res = await axios.get(`${API_SERVER_HOST}/api/member/kakao?accessToken=${accessToken}`)
+export const getMemberWithAccessToken = async (accessToken) => {
+  const res = await axios.get(
+    `${API_SERVER_HOST}/api/member/kakao?accessToken=${accessToken}`
+  );
 
-  return res.data
-
-}
+  return res.data;
+};
